1a984ea498c2ec0bb5c1232f8febdb98897b55c5
zzz <yuming5198@gmail.com>
2021-11-15 14:50:55 +0800

    update server

--- car/main.go
+++ car/main.go
@@ -3,33 +3,59 @@ package main
 import (
 	"crypto/md5"
 	"fmt"
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
 	"strings"
 )
 
 func main() {
-	s := "019230da18a700"
-	str := stringMD5(s)
+	router := gin.Default()
 
-	subStr := strings.ToUpper(str[1:7])
-	fmt.Println(subStr)
+	router.GET("/get-car-pwd", getCarPwd)
+
+	err := router.Run(":21111")
+	log.Fatalln(err)
+	//deviceId := "019230da18a700"
+
+}
+
+func getCarPwd(c *gin.Context) {
+	deviceId := c.Query("id")
+	if deviceId == "" {
+		c.String(http.StatusNotFound, "404 page not found")
+		return
+	}
+	str := stringMD5(deviceId)
+	pwd := generatePwd(str)
+	fmt.Println("deviceID:", deviceId, "; password:", pwd)
+
+	c.String(http.StatusOK, pwd)
+	return
+}
+
+func generatePwd(md5String string) string {
+	subStr := strings.ToUpper(md5String[1:7])
+	//fmt.Println(subStr)
 	var i int
 	var j int
 
-	paramString := ""
+	password := ""
 	for i = 0; i < len(subStr); i = j {
 		j = i + 1
 
 		c := subStr[i:j][0]
 
 		stringBuild := strings.Builder{}
-		stringBuild.WriteString(paramString)
+		stringBuild.WriteString(password)
 		tmp := c % 10 * uint8(i+2) % 10
 
 		stringBuild.WriteString(fmt.Sprintf("%d", tmp))
-		paramString = stringBuild.String()
+		password = stringBuild.String()
 	}
 
-	fmt.Println(paramString)
+	//fmt.Println(password)
+	return password
 }
 
 func stringMD5(param string) string {
@@ -39,7 +65,7 @@ func stringMD5(param string) string {
 	stringBuf := strings.Builder{}
 	for j := i; j < len(arrMD5); j++ {
 		i = int(arrMD5[j] & 0xFF)
-		fmt.Println(i)
+		//fmt.Println(i)
 		if i < 16 {
 			stringBuf.WriteString("0")
 		}
